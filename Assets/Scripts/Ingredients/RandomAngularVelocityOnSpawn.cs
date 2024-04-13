982e6a5c90409d55e441c76daf106236d66d48ff
René Löscher <rloescher@newmoonproduction.com>
2024-04-13 15:03:41 +0200

    added some physics material to cup and ingredients; added random rotation on spawning ingredients

---
+++ Assets/Scripts/Ingredients/RandomAngularVelocityOnSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using Random = Unity.Mathematics.Random;
+
+namespace PotatoFinch.LudumDare55.Ingredients {
+	public class RandomAngularVelocityOnSpawn : MonoBehaviour {
+		[SerializeField] private Rigidbody2D _rigidbody;
+		
+		private void Awake() {
+			Random random = new Random(1 + (uint)((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds());
+			random.NextInt(5);
+
+			_rigidbody.angularVelocity = random.NextFloat(-359, 359);
+		}
+	}
+}
