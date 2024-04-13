ca50ff0bf3c3049de2129dc31e2e36c9610a1f68
René Löscher <rloescher@newmoonproduction.com>
2024-04-13 13:24:35 +0200

    mapping input to random ingredient spawn

---
+++ Assets/Scripts/Extensions/ListExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Unity.Mathematics;
+
+namespace PotatoFinch.LudumDare55.Extensions {
+	public static class ListExtensions {
+		public static void Shuffle<T>(this List<T> list, Random random) {
+			int n = list.Count;
+			while (n > 1) {
+				n--;
+				int k = random.NextInt(n + 1);
+				(list[k], list[n]) = (list[n], list[k]);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/GameManagement/GameManagerBehaviour.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using PotatoFinch.LudumDare55.Extensions;
+using PotatoFinch.LudumDare55.Ingredients;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Random = Unity.Mathematics.Random;
+
+namespace PotatoFinch.LudumDare55.GameManagement {
+	public class GameManagerBehaviour : MonoBehaviour {
+		private SummoningGameInput _inputActions;
+
+		private Random _random;
+		private List<IngredientType> _randomizedIngredientTypeList;
+
+		private void Awake() {
+			_inputActions = new SummoningGameInput();
+			_inputActions.Enable();
+			_random = new Random(1 + (uint)((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds());
+			
+			RandomizeIngredientInputs();
+			AddListeners();
+		}
+
+		private void OnDestroy() {
+			RemoveListeners();
+		}
+
+		private void AddListeners() {
+			_inputActions.Summoning.Ingredient1.performed += OnIngredient1Performed;
+			_inputActions.Summoning.Ingredient2.performed += OnIngredient2Performed;
+			_inputActions.Summoning.Ingredient3.performed += OnIngredient3Performed;
+			_inputActions.Summoning.Ingredient4.performed += OnIngredient4Performed;
+		}
+
+		private void RemoveListeners() {
+			_inputActions.Summoning.Ingredient1.performed -= OnIngredient1Performed;
+			_inputActions.Summoning.Ingredient2.performed -= OnIngredient2Performed;
+			_inputActions.Summoning.Ingredient3.performed -= OnIngredient3Performed;
+			_inputActions.Summoning.Ingredient4.performed -= OnIngredient4Performed;
+		}
+
+		private void RandomizeIngredientInputs() {
+			IngredientType[] values = Enum.GetValues(typeof(IngredientType)) as IngredientType[];
+			_randomizedIngredientTypeList = new List<IngredientType>(values);
+			
+			_randomizedIngredientTypeList.Shuffle(_random);
+		}
+
+		private void OnIngredient1Performed(InputAction.CallbackContext _) {
+			IngredientSpawner.Instance.SpawnIngredientObjectAsync(_randomizedIngredientTypeList[0]);
+		}
+
+		private void OnIngredient2Performed(InputAction.CallbackContext _) {
+			IngredientSpawner.Instance.SpawnIngredientObjectAsync(_randomizedIngredientTypeList[1]);
+		}
+
+		private void OnIngredient3Performed(InputAction.CallbackContext _) {
+			IngredientSpawner.Instance.SpawnIngredientObjectAsync(_randomizedIngredientTypeList[2]);
+		}
+
+		private void OnIngredient4Performed(InputAction.CallbackContext _) {
+			IngredientSpawner.Instance.SpawnIngredientObjectAsync(_randomizedIngredientTypeList[3]);
+		}
+	}
+}
