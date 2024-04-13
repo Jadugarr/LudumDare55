4116b359ecb5f5db1d440e8bea967878fcb7e699
René Löscher <rloescher@newmoonproduction.com>
2024-04-13 13:57:20 +0200

    added button to discard

---
+++ Assets/Scripts/UiComponents/DiscardIngredientsButtonBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using PotatoFinch.LudumDare55.Ingredients;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PotatoFinch.LudumDare55.UiComponents {
+	public class DiscardIngredientsButtonBehaviour : MonoBehaviour {
+		[SerializeField] private Button _button;
+
+		private void OnEnable() {
+			_button.onClick.AddListener(OnDiscardButtonClicked);
+		}
+
+		private void OnDisable() {
+			_button.onClick.RemoveListener(OnDiscardButtonClicked);
+		}
+
+		private void OnDiscardButtonClicked() {
+			IngredientSpawner.Instance.DespawnAllIngredientObjects();
+		}
+	}
+}
